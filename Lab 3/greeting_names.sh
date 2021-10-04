066e7f7dbc87421a0adc03d46575bf4f74477b63
jinnguo <hg325@cornell.edu>
2021-10-04 16:14:57 -0400

    lab3

---
+++ Lab 3/greeting_names.sh
@@ -0,0 +1,4 @@
+#adapted from https://learn.adafruit.com/speech-synthesis-on-the-raspberry-pi/speak-easier-flite
+
+flite -voice slt -t "Good morning, Jinn. Oh and in case I don't see ya, good afternoon, good evening and good night!"
+
